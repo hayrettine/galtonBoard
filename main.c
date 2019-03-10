c003cebefa600ae4b4bf86e33d392689d235165b
hayrettine <hayrettine@anadolu.edu.tr>
2019-03-11 00:08:34 +0300

    reformat

--- main.c
+++ main.c
@@ -5,35 +5,40 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <zconf.h>
-int galton(void){
+
+int galton(void) {
     int sum = 0;
-    for (int i = 0; i < 19 ; i++) {
-        sum +=rand()%2;
+    for (int i = 0; i < 19; i++) {
+        sum += rand() % 2;
     }
     return sum;
 }
-pthread_rwlock_t  rwlock = PTHREAD_RWLOCK_INITIALIZER;
+
+pthread_rwlock_t rwlock = PTHREAD_RWLOCK_INITIALIZER;
+
 void *cell();
-int array[20] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+int array[20] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 pthread_mutex_t mutex_array[20];
-void print_cells(){
+
+void print_cells() {
     printf("Cell:  Value\n------------\n");
-    for (int i = 0; i < 20 ; i++) {
-        if (i < 10) printf("   %i:  %i\n",i,array[i]);
-        else printf("  %i:  %i\n",i,array[i]);
+    for (int i = 0; i < 20; i++) {
+        if (i < 10) printf("   %i:  %i\n", i, array[i]);
+        else printf("  %i:  %i\n", i, array[i]);
     }
 }
 
-int sumOfCells(){
+int sumOfCells() {
     int sum = 0;
-    for (int i = 0; i < 20 ; i++) {
+    for (int i = 0; i < 20; i++) {
         sum += array[i];
     }
     return sum;
 }
 
-int main () {
-    for(int i = 0; i<20;i++){
+int main() {
+    for (int i = 0; i < 20; i++) {
         pthread_mutex_init(&mutex_array[i], NULL);
     }
 
@@ -45,16 +50,16 @@ int main () {
     int success = 0;
     srand(time(0));
 
-    for(int i=0;i<1000000;i++) {
+    for (int i = 0; i < 1000000; i++) {
 
-        decision = pthread_create(&tids[i], &attr , &cell, NULL);
+        decision = pthread_create(&tids[i], &attr, &cell, NULL);
 
-        if (decision == 0){
+        if (decision == 0) {
             success++;
         }
     }
 
-    for (int j = 0; j < success ; j++) {
+    for (int j = 0; j < success; j++) {
         pthread_join(tids[j], NULL);
     }
 
@@ -63,10 +68,11 @@ int main () {
     printf("Sum of values in the cell: %i", sumOfCells());
     return 0;
 }
-void *cell(){
+
+void *cell() {
     int index = galton();
-    while (pthread_mutex_trylock(&mutex_array[index])!=0);
-            array[index]++;
-            pthread_mutex_unlock(&mutex_array[index]);
+    while (pthread_mutex_trylock(&mutex_array[index]) != 0);
+    array[index]++;
+    pthread_mutex_unlock(&mutex_array[index]);
 
 }
